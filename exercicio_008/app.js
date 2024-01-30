cf5bb4734345729e6d6ffe9ea22365d52ad52636
gabriel8programmer <gabriel8github@gmail.com>
2024-01-30 18:55:17 -0300

    exercicie 8 - ok

--- exercicio_008/app.js
+++ exercicio_008/app.js
@@ -6,3 +6,21 @@ Enunciado:
     A ideia é criar o código javascript que permita que o clique em cada
     um dos botões altere o texto acima para a cor de fundo do botão.
 ---------------------------------------------------------------------------- */
+
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4");
+const btn5 = document.querySelector("#btn5");
+const text = document.querySelector("h3");
+
+const changeTextColor = (e)=> {
+    const bgColor = getComputedStyle(e.target).backgroundColor;
+    text.style.color = bgColor;
+}
+
+btn1.addEventListener("click", changeTextColor);
+btn2.addEventListener("click", changeTextColor);
+btn3.addEventListener("click", changeTextColor);
+btn4.addEventListener("click", changeTextColor);
+btn5.addEventListener("click", changeTextColor);
