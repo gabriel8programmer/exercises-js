2179500e6e9d47f9ce5b4dd93f82d173861e7b9e
gabriel8programmer <gabriel8github@gmail.com>
2024-01-29 21:45:51 -0300

    exercise 2 - ok

--- exercicio_002/app.js
+++ exercicio_002/app.js
@@ -8,3 +8,18 @@ Enunciado: CONTADOR COM INTERVALO DE VALORES
     O valor nunca poderá ser inferior a -10 ou superior a 10.
 
 ---------------------------------------------------------------------------- */
+
+const btnDecrement = document.querySelector("#btn_decremento");
+const btnIncrement = document.querySelector("#btn_incremento");
+const value = document.querySelector("#valor");
+
+btnDecrement.addEventListener("click", ()=> {
+    if (parseInt(value.innerText) <= -10) return;
+    value.innerText = parseInt(value.innerText)-1;
+});
+
+btnIncrement.addEventListener("click", ()=> {
+    if (parseInt(value.innerText) >= 10) return;
+    value.innerText = parseInt(value.innerText)+1;
+});
+
