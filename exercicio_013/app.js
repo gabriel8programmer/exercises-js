a19e8d8f3c85c959cccef129142ea985ff1c7112
gabriel8programmer <gabriel8github@gmail.com>
2024-01-30 19:51:00 -0300

    exercicie 13 - ok

--- exercicio_013/app.js
+++ exercicio_013/app.js
@@ -7,3 +7,21 @@ Enunciado:
     sequencial e depois de inserido, o input deve ser limpo e receber o focus para
     nova inserção.
 ---------------------------------------------------------------------------- */
+
+const text_post = document.querySelector("#text_post");
+const button = document.querySelector("button");
+const posts = document.querySelector("#posts");
+
+const addInPost = ()=> {
+    if (text_post.value != ""){
+        const p = document.createElement("p");
+        const listP = posts.querySelectorAll("p");
+        p.innerText = `${listP.length+1} - ${text_post.value}`;
+
+        posts.appendChild(p);
+        text_post.value = "";
+        text_post.focus();
+    }
+}
+
+button.addEventListener("click", addInPost);
