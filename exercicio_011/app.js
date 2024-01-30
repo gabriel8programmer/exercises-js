990701bd30b36a6acd21b45b21577a47f4cb8ccb
gabriel8programmer <gabriel8github@gmail.com>
2024-01-30 19:34:21 -0300

    exercicie 11 - ok

--- exercicio_011/app.js
+++ exercicio_011/app.js
@@ -2,9 +2,39 @@
 
 Exercício: 011
 Enunciado:
-    A grelha inicial contém dezasseis imgs sem souce.
+    A grelha inicial contém dezesseis imgs sem souce.
     Quando clicamos numa das imgs, vamos apresentar o símbolo X (imagem)
     No clique seguinte, que só pode acontecer numa img que não tenha símbolo,
     vamos apresentar o símbolo 'O'.
 ---------------------------------------------------------------------------- */
+
+const getImgs = () => {
+    const images = [];
+    for (let c = 1; c <= 16; c++) {
+        const id = `img${c}`;
+        const el = document.querySelector(`#${id}`);
+        images.push(el);
+    }
+
+    return images;
+}
+
+//get all images
+const images = getImgs();
+const possibleImgs = ["x.png", "o.png"];
+
+let indexImg = 0;
+
+const updateGrid = (e) => {
+    const el = e.target;
+
+    if (el.getAttribute("src") === null){
+        el.setAttribute("src", possibleImgs[indexImg]);
+        indexImg = (indexImg === 1) ? 0 : 1;
+    }
+}
+
+images.forEach(img => {
+    img.addEventListener("click", updateGrid);
+});
 
