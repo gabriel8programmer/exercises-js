e60484e76c556895639a9e8f167b128742a772af
gabriel8programmer <gabriel8github@gmail.com>
2024-01-30 18:45:21 -0300

    exercicie 7 - ok

--- exercicio_007/app.js
+++ exercicio_007/app.js
@@ -1,6 +1,27 @@
 /* ----------------------------------------------------------------------------
 
-Exercício: 006
+Exercício: 007
 Enunciado:
     O mesmo exercício do vídeo anterior, mas com código mais sintético.
 ---------------------------------------------------------------------------- */
+
+const tabs = document.querySelectorAll("#tab1, #tab2, #tab3");
+const infos = document.querySelectorAll("#info1, #info2, #info3");
+
+const hideInfos = ()=> {
+    infos.forEach(info => {
+        info.classList.remove("d-block");
+        info.classList.add("d-none");
+    });
+}
+
+const showInfo = (index)=> {
+    hideInfos();
+    const info = infos[index];
+    info.classList.add("d-block");
+    info.classList.remove("d-none");
+}
+
+tabs.forEach((tab, indexTab) => {
+    tab.addEventListener("click", ()=> { showInfo(indexTab) });
+});
