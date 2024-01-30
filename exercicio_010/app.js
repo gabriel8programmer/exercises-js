39dcf830e35a3c7e972683692fa7c68574a19cee
gabriel8programmer <gabriel8github@gmail.com>
2024-01-30 19:08:59 -0300

    exercicie 10 - ok

--- exercicio_010/app.js
+++ exercicio_010/app.js
@@ -7,4 +7,22 @@ Enunciado:
     amarelo e as restantes a vermelho.
 ---------------------------------------------------------------------------- */
 
+const boxes = document.querySelectorAll(".box");
 
+const resetAllBoxes = ()=> {
+    boxes.forEach(box => {
+        box.classList.remove("bg-warning");
+        box.classList.add("bg-danger");
+    })
+}
+
+const changeFlowBox = (e)=> {
+    resetAllBoxes();
+    const element = e.target;
+    element.classList.remove("bg-danger");
+    element.classList.add("bg-warning");
+}
+
+boxes.forEach(box => {
+    box.addEventListener("click", changeFlowBox);
+})
