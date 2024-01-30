aa9b684a1677a239af70eaf3c0d74b8bbe863eea
gabriel8programmer <gabriel8github@gmail.com>
2024-01-29 22:11:38 -0300

    exercise 3 - ok

--- exercicio_003/app.js
+++ exercicio_003/app.js
@@ -10,3 +10,28 @@ Enunciado: CONTADOR COM CORES PARA NEGATIVOS, ZERO E POSITIVOS
     No caso do valor ser positivo o texto deve ser verde puro.
 
 ---------------------------------------------------------------------------- */
+
+const btnDecrement = document.querySelector("#btn_decremento");
+const btnIncrement = document.querySelector("#btn_incremento");
+const value = document.querySelector("#valor");
+
+function updateColorValue(currentValue) {
+    value.removeAttribute("class");
+    if (currentValue === 0){
+        value.classList.add("text-white");
+    } else if (currentValue < 0){
+        value.classList.add("text-danger");
+    } else {
+        value.classList.add("text-success");
+    }
+}
+
+btnDecrement.addEventListener("click", ()=> {
+    value.innerText = parseInt(value.innerText)-1;
+    updateColorValue(parseInt(value.innerText));
+});
+
+btnIncrement.addEventListener("click", ()=> {
+    value.innerText = parseInt(value.innerText)+1;
+    updateColorValue(parseInt(value.innerText));
+});
