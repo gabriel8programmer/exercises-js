ee2d46d0696320691298d0b14362b2a94076144e
gabriel8programmer <gabriel8github@gmail.com>
2024-02-02 20:16:03 -0300

    exercise 17 - ok

--- exercicio_017/app.js
+++ exercicio_017/app.js
@@ -7,3 +7,39 @@ Enunciado:
    
     Não pode ser adicionada uma palavra ou frase repetida.
 ---------------------------------------------------------------------------- */
+
+const text_post = document.querySelector("#text_post");
+const button = document.querySelector("button");
+const posts = document.querySelector("#posts");
+
+const alreadyExistsInPosts = (text)=> {
+    const ps = posts.querySelectorAll("p");
+
+    for (let p of ps){
+        if (p.innerText == text){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+const resetInput = ()=> {
+    //clear input and set focus
+    text_post.value = "";
+    text_post.focus();
+}
+
+const addNewPost = ()=> {
+    if (text_post.value != "" && !alreadyExistsInPosts(text_post.value)){
+        const p = document.createElement("p");
+        p.innerText = text_post.value;
+        posts.appendChild(p);
+        resetInput();
+        return;
+    } 
+
+    resetInput();
+}
+
+button.addEventListener("click", addNewPost);
