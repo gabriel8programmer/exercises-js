46ddc97fcd1f31d439b5bfd756101750a2aa3e26
gabriel8programmer <gabriel8github@gmail.com>
2024-02-02 10:58:57 -0300

    exercise 15 - ok

--- exercicio_015/app.js
+++ exercicio_015/app.js
@@ -7,3 +7,23 @@ Enunciado:
 
     As palavras e frases mais recentes devem ficar no topo.
 ---------------------------------------------------------------------------- */
+
+const text_post = document.querySelector("#text_post");
+const button = document.querySelector("button");
+const posts = document.querySelector("#posts");
+
+const addPostBefore = ()=> {
+    if (text_post.value != ""){
+        const p = document.createElement("p");
+        p.innerText = text_post.value;
+        
+        const referenceElement = posts.children[0];
+        posts.insertBefore(p, referenceElement);
+
+        //clear input and set focus
+        text_post.value = "";
+        text_post.focus();
+    }
+}
+
+button.addEventListener("click", addPostBefore);
