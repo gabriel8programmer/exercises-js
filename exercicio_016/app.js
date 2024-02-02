f5e2d2bf8a7c0bf6359d2407c9531244372296ae
gabriel8programmer <gabriel8github@gmail.com>
2024-02-02 11:09:53 -0300

    exercise 16 - ok

--- exercicio_016/app.js
+++ exercicio_016/app.js
@@ -11,4 +11,37 @@ Enunciado:
     NOTA: O HTML não pode ser alterado.
 ---------------------------------------------------------------------------- */
 
+const text_post = document.querySelector("#text_post");
+const buttons = document.querySelectorAll("button");
+const posts = document.querySelector("#posts");
+
+const btnClear = buttons[0];
+const btnAdd = buttons[1];
+
+//functions
+const resetInput = ()=> {
+    //clear input and set focus
+    text_post.value = "";
+    text_post.focus();
+}
+
+const clearAll = ()=> {
+    //clear posts
+    posts.innerHTML = "";
+    resetInput();
+}
+
+const addNewPost = ()=> {
+    if (text_post.value != ""){
+        //create element p
+        const p = document.createElement("p");
+        p.innerText = text_post.value;
+        //add in posts
+        posts.appendChild(p);
+        resetInput();
+    }
+}
+
+btnClear.addEventListener("click", clearAll);
+btnAdd.addEventListener("click", addNewPost);
 
