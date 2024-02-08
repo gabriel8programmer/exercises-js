1a688ded55c5bed9aa57411e5d8949678e426d21
gabriel8programmer <gabriel8github@gmail.com>
2024-02-07 22:37:01 -0300

    exercise 25 - ok

--- exercicio_025/app.js
+++ exercicio_025/app.js
@@ -7,3 +7,39 @@ Enunciado:
     no div cujo id = resultados.
 ---------------------------------------------------------------------------- */
 
+const text_numero = document.querySelector("#text_numero");
+const resultados = document.querySelector("#resultados");
+
+const defineDefaultMaxMin = (min, max)=> {
+    text_numero.setAttribute("min", min);
+    text_numero.setAttribute("max", max);
+}
+
+const showCurrentMultTable = (e)=> {
+
+    //get value
+    const value = parseInt(e.target.value);
+
+    //clear results
+    resultados.innerHTML = "";
+
+    //check if the value is right
+    if (value < 1 || value > 20){
+        resultados.innerText = "[resultados]";
+        text_numero.value = 1;
+        return;
+    }
+
+    for (let c = 0; c <= 10; c++){
+        //create element and add in div of the results
+        const p = document.createElement("p");
+        p.innerText = `${value} x ${c} = ${value*c}`;
+        resultados.appendChild(p);
+    }
+}
+
+text_numero.addEventListener("change", showCurrentMultTable);
+
+//define max value in input
+defineDefaultMaxMin(1, 20);
+
