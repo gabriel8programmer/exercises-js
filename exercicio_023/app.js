736339600d1d26a1c34f6b100a6906561eb45d31
gabriel8programmer <gabriel8github@gmail.com>
2024-02-07 21:51:08 -0300

    exercise 23 - ok

--- exercicio_023/app.js
+++ exercicio_023/app.js
@@ -8,3 +8,18 @@ Enunciado:
     Ao clicar em todas, todas as checkboxes devem ser checkadas.
     Ao clicar em nenhuma, todas as checkboxes devem ser descheckadas.
 ---------------------------------------------------------------------------- */
+
+const checks = document.querySelectorAll("#check_1,#check_2,#check_3,#check_4");
+const all = document.querySelector("#all");
+const none = document.querySelector("#none");
+
+const markAllChecks = ()=> {
+    checks.forEach(check => check.setAttribute("checked", true));
+}
+
+const desmarkAllChecks = ()=> {
+    checks.forEach(check => check.removeAttribute("checked"));
+}
+
+all.addEventListener("click", markAllChecks);
+none.addEventListener("click", desmarkAllChecks);
