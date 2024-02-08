91dc1e6490311dd4470195bfe0a98f7ff2c613b0
gabriel8programmer <gabriel8github@gmail.com>
2024-02-08 20:27:42 -0300

    exercicio 28 - ok

--- exercicio_028/app.js
+++ exercicio_028/app.js
@@ -6,3 +6,19 @@ Enunciado:
     Esses números devem ser apresentados cada um em sua própria linha dentro do elemento
     cujo id é igual a "numeros".
 ---------------------------------------------------------------------------- */
+
+const button = document.querySelector("button");
+const numeros = document.querySelector("#numeros");
+
+const addRandomNumbers = ()=> {
+    numeros.innerHTML = "";
+    //create the 20 elements for the numbers
+    for (let c = 0; c < 20; c++){
+        const currentRandomValue = Math.floor(Math.random() * 1000)+1; 
+        const num = document.createElement("div");
+        num.innerText = currentRandomValue;
+        numeros.appendChild(num);
+    }
+}
+
+button.addEventListener("click", addRandomNumbers);
