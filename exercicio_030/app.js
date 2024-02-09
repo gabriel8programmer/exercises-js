5430fb3708a190a86e5dfdecf2f0ec97a6e379c7
gabriel8programmer <gabriel8github@gmail.com>
2024-02-08 21:09:04 -0300

    exercicio 30 - ok

--- exercicio_030/app.js
+++ exercicio_030/app.js
@@ -9,3 +9,23 @@ Enunciado:
     Se o resultado de qualquer cálculo for incorreto, deve aparecer a mensagem
     "Valor inválido"
 ---------------------------------------------------------------------------- */
+
+const text_valor = document.querySelector("#text_valor");
+const button = document.querySelector("button");
+const resultado = document.querySelector("#resultado");
+
+const showResult = ()=> {
+    const value = text_valor.value;
+
+    //get final value
+    let finalValue = +(value) / 3;
+    finalValue = finalValue.toFixed(1);
+
+    if (value == "" || finalValue == "NaN"){
+        finalValue = "Valor inválido";
+    }
+
+    resultado.innerHTML += `<div>${finalValue}</div>`;
+}
+
+button.addEventListener("click", showResult);
